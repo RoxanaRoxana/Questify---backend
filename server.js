71872b5a52834c2abb7c3ee65731e33919c7067f
Radek <radekie@wp.pl>
2022-06-12 18:16:50 +0200

    cors update 2

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 
-app.use("*", cors("*"));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 const swaggerOptions = {
   definition: {
